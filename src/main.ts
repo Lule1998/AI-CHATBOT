6d9a83e5918280cdbf1248f12f20b0f9ce5607c5
Luka <167115889+Lule1998@users.noreply.github.com>
2024-11-12 14:23:10 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-// src/main.ts
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
